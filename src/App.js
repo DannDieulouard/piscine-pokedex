6b65d016a46f2d6e397d66a04cb0401538fdba5c
Dann Dieulouard <dann.dieulouard@lapiscine.pro>
2024-04-04 09:56:30 +0200

    project without props

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Header from './Header';
+import Footer from './Footer';
+import LastCapturedPokemons from './LastCapturedPokemons';
+import RandomPokemon from './RandomPokemon';
+import FirePokemons from './FirePokemons';
+
+function App() {
+  return (
+    <>
+    <Header />
+    <LastCapturedPokemons />
+    <FirePokemons />
+    <RandomPokemon />
+    <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer>
+            <p>Pokemons</p>
+        </footer>
+    )
+}
+
+export default Footer;
